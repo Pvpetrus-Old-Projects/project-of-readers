1330eaad15ba692dc56673944fde0bf0e0a6915a
Jakub Kowalewski <81765492+kevvol@users.noreply.github.com>
2022-05-16 15:05:57 +0200

    dzialajace parametry

--- projekt.c
+++ projekt.c
@@ -4,47 +4,68 @@
 #include <unistd.h>
 #include <syslog.h>
 
+
 pthread_mutex_t blokadaCzytelnikow;
 pthread_mutex_t blokadPisarzy;
 
 
-int main(int arg,char **argv)
+int main(int argc, char *argv[])
 {
 
-    char readers;
-    char writers;
     int liczba_czytelnikow;
     int liczba_pisarzy;
-
-    if(argc!=2) {
-        printf("\n Zla liczba parametrow");
-	    retrun 0;
+	int nr_blednego_parametru = 0;
+	
+    if(argc!=3) {
+        printf("Zla liczba parametrow\n");
+		printf("Wprowadz parametry jako liczby calkowite oddzielone spacja, np: 1 2 ");
+	    return 0;
     }
     else
     {
-	    if(atoi(argv[0]) != 0)
+	    if(atoi(argv[1]) != 0)
 	    {
-		    liczba_czytelenikow = atoi(argv[0]);
-		    printf("Ilośc czytleników wynosi: %d.", ilosc_czytlenikow); 
+		    liczba_czytelnikow = atoi(argv[1]);
+		    //printf("Ilośc czytleników wynosi: %d.", liczba_czytelnikow); 
 	    }
-	    else if(argv[0]) == 0)
+	    if(atoi(argv[1]) == 0)
 	    {
-		    printf("Bledny parametr nr 1!");
-		    return 0;
+		    //printf("Bledny parametr nr 1!");
+			nr_blednego_parametru = nr_blednego_parametru + 1;
+		    //return 0;
 	    }
-	    else if(atoi(argv[1]) != 0)
+	    if(atoi(argv[2]) != 0)
 	    {
-		    liczba_pisarzy = atoi(argv[1]);
-		    printf("Ilośc pisarzy wynosi: %d.", ilosc_pisarzy); 
+		    liczba_pisarzy = atoi(argv[2]);
+		    printf("Ilośc pisarzy wynosi: %d.", liczba_pisarzy); 
 	    }
-	    else
+	    if(atoi(argv[2]) == 0)
 	    {
-		    printf("Bledny parametr nr 2!");
-		    return 0;
+		    //printf("Bledny parametr nr 2!");
+			nr_blednego_parametru = nr_blednego_parametru + 2;
+		    //return 0;
 	    }
+		if(nr_blednego_parametru != 0)
+		{
+			if(nr_blednego_parametru == 1)
+			{
+				printf("Bledny parametr nr 1!");
+				return 0;
+			}
+			else if(nr_blednego_parametru == 2)
+			{
+				printf("Bledny parametr nr 2!");
+				return 0;
+			}
+			else
+			{
+				printf("Niepoprawne oba parametry!");
+				return 0;
+			}
+		}
     
     }
-	printf("Czytelnicy: %d, pisarze %d", liczba_czytelnikow, liczba pisarzy);
+	printf("Czytelnicy: %d, pisarze %d", liczba_czytelnikow, liczba_pisarzy);
         
 	return 0;
 	
